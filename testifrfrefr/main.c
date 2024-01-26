7f924cddb33de6ec119043a27130ad4f3c658aa8
Braltzu <156204503+Braltzu@users.noreply.github.com>
2024-01-26 16:40:03 +0200

    Add files via upload

---
+++ testifrfrefr/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+
+        int korkeus, kanta, sum;
+        //float korkeus, kanta;
+
+        printf("anna kolmion korkeus ja kanta: ");
+        scanf("%d %d",&korkeus,&kanta);
+
+
+        //pinta alan laskuri
+
+        sum= korkeus * kanta;
+
+        printf("%d * %d = %d", korkeus, kanta, sum);
+
+
+
+
+
+
+
+
+    return 0;
+}
